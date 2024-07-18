f1e9d5fdda326b466ec03c607f03293f35e16336
vignesh <vignesh15499@gmail.com>
2024-07-18 22:55:26 +0530

    user clone changes

--- readWrite.py
+++ readWrite.py
@@ -16,16 +16,6 @@ file = open("testData.txt")
 
 # another way
 # git example
-# print(file.read())  # using read() we can read all the content of the respective file
-
-# print(file.read(2))  # reads only the specific size
-
-# print(file.readline())  # reads only single line
-# print(file.readline())
-
-# read the file line by line using while loop
-#line = file.readline()
-#while line != "":
 #    print(line)
 #    line = file.readline()
 listContent = file.readlines()  # this function stores all the contents inside the list
